cca52bc1047c22afbd61fc19323564b49e341d01
oscarjohnson04 <oscarlloydjohnson@gmail.com>
2025-09-16 15:56:02 -0400

    nkmodel.py

--- nkmodel.py
+++ nkmodel.py
@@ -25,7 +25,7 @@ with col3:
   real_interest_rate = float(user_real_interest_rate)
   user_phi_y = st.text_input("Enter CB coefficient for Output Gap", "0.5")
   phi_y = float(user_phi_y)
-  T = st.number_input("Enter simulation periods", "50")
+  T = st.number_input("Enter simulation periods", 50)
   
 for t in range(T-1):
     # Expectations = last period values (simple approximation)
