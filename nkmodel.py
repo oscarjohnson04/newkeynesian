4a80c5aaff459c61cbad91b8493a61627b97d99d
oscarjohnson04 <oscarlloydjohnson@gmail.com>
2025-09-16 21:32:20 -0400

    nkmodel.py

--- nkmodel.py
+++ nkmodel.py
@@ -32,7 +32,7 @@ st.sidebar.header("Shock Settings")
 shock_location = st.sidebar.selectbox("Shock affects", ["Phillips Curve (Supply Shock)", "IS Curve (Demand Shock)"])
 shock_type = st.sidebar.selectbox("Select shock type", ["None", "Single", "Persistent"])
 if shock_type != "None":
-  shock_size = st.sidebar.number_input("Shock size (%)", -100.0, 100.0, 1.0) / 100
+  shock_size = st.sidebar.number_input("Shock size (%)", -100.0, 100.0, 1.0)
   shock_time = st.sidebar.number_input("Shock start period", min_value=0, max_value=T-1, value=0, step=1)
   if shock_type == "Persistent":
     shock_duration = st.sidebar.number_input("Shock duration", min_value=0, max_value=T-1, value=0, step=1)
